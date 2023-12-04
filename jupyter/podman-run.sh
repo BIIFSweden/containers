63297a02bdb94e9aa88410bf1558a23c9b499a37
Jonas Windhager <jonas.windhager@scilifelab.se>
2023-12-04 19:55:59 +0100

    add tissuumaps

--- jupyter/podman-run.sh
+++ jupyter/podman-run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-podman run -p 8888:8888 ghcr.io/biifsweden/jupyter:latest
+podman run -p 8888:8888 ghcr.io/biifsweden/jupyter
---
+++ tissuumaps/podman-run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+podman run -p 56733:80 ghcr.io/biifsweden/tissuumaps
